6f0e598a323b6a0fbb9671a47440419798413af3
woogieReal <woogiereal@gmail.com>
2022-09-29 19:46:39 +0900

    [라우팅] 앱 진입 시 /tree로 이동하도록 설정

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,16 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 const Home: NextPage = () => {
+  const router = useRouter()
+
+  useEffect(() => {
+    router.replace('/tree');
+  }, [])
+
   return (
     <div className={styles.container}>
       <Head>
