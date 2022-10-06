d944715392d54b18cc2c6921d6ce30ededa0276c
woogieReal <woogiereal@gmail.com>
2022-10-06 17:02:49 +0900

    [트리] drawer 섹션 생성

--- components/tree/sections/treeSection.tsx
+++ components/tree/sections/treeSection.tsx
@@ -1,11 +1,78 @@
-import Grid from "@mui/material/Grid";
+import * as React from 'react';
+import { useTheme } from '@mui/material/styles';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import IconButton from '@mui/material/IconButton';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import InboxIcon from '@mui/icons-material/MoveToInbox';
+import MailIcon from '@mui/icons-material/Mail';
+import DrawerHeader from '@/components/tree/modules/drawerHeader';
+import { MouseEventHandler } from 'react';
 
-const TreeSection = () => {
+interface Props {
+  open: boolean;
+  drawerWidth: number;
+  setDrawerWidth: Function;
+  handleDrawerClose: MouseEventHandler;
+}
+const TreeSection = ({ open, drawerWidth, setDrawerWidth, handleDrawerClose }: Props) => {
+  const theme = useTheme();
 
   return (
-    <Grid container item>
-
-    </Grid>
+    <Drawer
+      sx={{
+        width: drawerWidth,
+        flexShrink: 0,
+        '& .MuiDrawer-paper': {
+          width: drawerWidth,
+          boxSizing: 'border-box',
+        },
+      }}
+      variant="persistent"
+      anchor="left"
+      open={open}
+      PaperProps={{ style: { width: drawerWidth } }}
+    >
+      <DrawerHeader>
+        <IconButton onClick={handleDrawerClose}>
+          {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+        </IconButton>
+      </DrawerHeader>
+      <Divider />
+      <List>
+        {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton onClick={() => {
+              setDrawerWidth(100 * 2 * (index + 1));
+            }}>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {['All mail', 'Trash', 'Spam'].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+              </ListItemIcon>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Drawer>
   )
 }
 
--- pages/tree/index.tsx
+++ pages/tree/index.tsx
@@ -1,15 +1,37 @@
 import TreeSection from '@/components/tree/sections/treeSection'
 import ViewSection from '@/components/tree/sections/viewSection'
-import { Grid } from '@mui/material'
+import { Box, Grid } from '@mui/material'
 import type { NextPage } from 'next'
+import { useCallback, useState } from 'react'
+
+const MIN_DRAWER_WIDTH = 240;
 
 const Home: NextPage = () => {
+  const [open, setOpen] = useState<boolean>(false);
+  const [drawerWidth, setDrawerWidth] = useState<number>(MIN_DRAWER_WIDTH);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
 
   return (
-    <Grid>
-      <TreeSection/>
-      <ViewSection/>
-    </Grid>
+    <Box sx={{ display: 'flex' }}>
+      <TreeSection
+        open={open}
+        drawerWidth={drawerWidth}
+        setDrawerWidth={setDrawerWidth}
+        handleDrawerClose={handleDrawerClose}
+      />
+      <ViewSection
+        open={open}
+        drawerWidth={drawerWidth}
+        handleDrawerOpen={handleDrawerOpen}
+      />
+    </Box>
   )
 }
 
