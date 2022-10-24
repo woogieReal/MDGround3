1657b6f08332a1968205eb5a6563ba651fd1a969
woogieReal <woogiereal@gmail.com>
2022-10-24 15:15:42 +0900

    [트리] 마이그레이션 파일 변경

--- src/sqls/config/0_0_1-CREATE_MIGRATION_TABLE.sql
+++ src/sqls/config/0_0_1-CREATE_MIGRATION_TABLE.sql
@@ -1,8 +1,8 @@
-CREATE TABLE migration (
+CREATE TABLE IF NOT EXISTS  migration (
 	migration_id varchar(50) NOT NULL COMMENT '마이그레이션 ID',
 	migration_name varchar(200) NOT NULL COMMENT '마이그레이션 이름',
 	migration_file_name varchar(250) NOT NULL COMMENT '마이그레이션 파일 이름',
-	checksum INT NOT NULL,
+	checksum CHAR(32) NOT NULL,
 	creation_datetime DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL COMMENT '실행 일시'
 )
 ENGINE=InnoDB
