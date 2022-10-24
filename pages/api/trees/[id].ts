e4b4eb96ab790e79b07216a64070bb4dc765fcdb
woogieReal <woogiereal@gmail.com>
2022-10-24 18:10:07 +0900

    [트리] 리스트, 개별조회 API 생성

--- pages/api/trees/[id].ts
+++ pages/api/trees/[id].ts
@@ -1,9 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { Tree } from "@/src/models/tree.model";
-import { mockGetTreeData } from "@/tests/tree/mockData";
-
-// Fake trees data
-const eachTree: Tree[] = mockGetTreeData;
+import DBConnection from "@/src/apis/dbConnection";
+import { Connection, RowDataPacket } from "mysql2/promise";
 
 export default function handler(_req: NextApiRequest, res: NextApiResponse) {
   const {
@@ -14,9 +11,26 @@ export default function handler(_req: NextApiRequest, res: NextApiResponse) {
 
   switch (method) {
     case "GET":
-      res
-        .status(200)
-        .json(eachTree.find((tree: Tree) => tree.treeId === Number(id)));
+      DBConnection.transactionExecutor(async (connection: Connection) => {
+        const [rows, fields] = await connection.query(`
+          SELECT 
+              tree_id AS treeId
+            , tree_type AS treeType
+            , tree_name AS treeName
+            , tree_content AS treeContent
+            , tree_path AS treePath
+            , delete_yn AS deleteYn
+            , created_datetime AS createdDatetime
+            , updated_datetime AS updatedDatetime
+            , deleted_datetime AS deletedDatetime
+          FROM tree
+          WHERE 1 = 1
+          AND delete_yn = 'N'
+          AND tree_id = ?
+          ORDER BY tree_path
+        `, [id]);
+        res.status(200).json((rows as RowDataPacket[])[0]);
+      });
       break;
     case "PUT":
       res.status(200).json(body);
--- pages/api/trees/index.ts
+++ pages/api/trees/index.ts
@@ -1,16 +1,27 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import { Tree } from "@/src/models/tree.model";
-import { mockGetTreesData } from "@/tests/tree/mockData";
-
-// Fake trees data
-const trees: Tree[] = mockGetTreesData;
+import DBConnection from "@/src/apis/dbConnection";
+import { Connection } from "mysql2/promise";
 
 export default function handler(_req: NextApiRequest, res: NextApiResponse) {
   const { body, method } = _req;
 
   switch (method) {
     case "GET":
-      res.status(200).json(trees);
+      DBConnection.transactionExecutor(async (connection: Connection) => {
+        const [rows, fields] = await connection.query(`
+          SELECT 
+              tree_id AS treeId
+            , tree_type AS treeType
+            , tree_name AS treeName
+            , tree_path AS treePath
+            , delete_yn AS deleteYn
+          FROM tree
+          WHERE 1 = 1
+          AND delete_yn = 'N'
+          ORDER BY tree_path
+        `);
+        res.status(200).json(rows);
+      });
       break;
     case "POST":
       res.status(200).json(body);
