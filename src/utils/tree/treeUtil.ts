b55aa650a919d88d57379237486364d09147f089
woogieReal <woogiereal@gmail.com>
2022-11-12 21:59:43 +0900

    [트리] createTreeFullPath null 로직 추가

--- src/utils/tree/treeUtil.ts
+++ src/utils/tree/treeUtil.ts
@@ -1,5 +1,11 @@
 import { Tree } from "@/src/models/tree.model";
 
-export const createTreeFullPath = (tree: Tree): string => {
-  return tree.treePath ? tree.treePath + '|' + tree.treeId : String(tree.treeId);
-}
+export const createTreeFullPath = (tree?: Tree): string => {
+  if (tree) {
+    return tree.treePath
+      ? tree.treePath + "|" + tree.treeId
+      : String(tree.treeId);
+  } else {
+    return "";
+  }
+};
