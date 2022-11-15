effd4226509ec2bf05a93b6ccb08137414ea6892
woogieReal <woogiereal@gmail.com>
2022-11-15 12:21:17 +0900

    [트리] 삭제 트리 state에서 제거 유틸 메소드 생성

--- src/utils/common/arrayUtil.ts
+++ src/utils/common/arrayUtil.ts
@@ -1,3 +1,7 @@
 export const removeTargetIndexDataFromArray = (trees: any[], targetIndex: number) => {
   return trees.filter((tree: any, index: number) => index !== targetIndex);
 }
+
+export const getEmptyArrayIfNotArray = (target: any) => {
+  return Array.isArray(target) ? target : [];
+}
--- src/utils/tree/treeUtil.ts
+++ src/utils/tree/treeUtil.ts
@@ -1,4 +1,6 @@
-import { Tree } from "@/src/models/tree.model";
+import { InitialTree, Tree } from "@/src/models/tree.model";
+import { cloneDeep } from "lodash";
+import { getEmptyArrayIfNotArray } from "../common/arrayUtil";
 
 export const createTreeFullPath = (tree?: Tree): string => {
   if (tree) {
@@ -9,3 +11,21 @@ export const createTreeFullPath = (tree?: Tree): string => {
     return "";
   }
 };
+
+export const deleteTreeFromTrees = (trees: Tree[], targetTree: Tree) => {
+  if (targetTree.treePath.length === 0) {
+    trees = trees.filter((child: Tree) => child.treeId !== targetTree.treeId);
+  } else {
+    const upperTreeIds = targetTree.treePath.split("|");
+    let targetUpperTree: Tree = { ...InitialTree, treeChildren: trees };
+
+    upperTreeIds.forEach((id: string) => {
+      targetUpperTree = targetUpperTree?.treeChildren?.find((upperTree: Tree) => upperTree.treeId === Number(id))!;
+    })
+
+    targetUpperTree.treeChildren = targetUpperTree.treeChildren?.filter((child: Tree) => child.treeId !== targetTree.treeId);
+  }
+
+  return cloneDeep(trees);
+}
+
