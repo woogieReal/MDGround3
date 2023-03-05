d08e8091ac6fac2e1ec3016e156d096cb62e8523
woogieReal <woogiereal@gmail.com>
2023-03-05 19:35:52 +0900

    [fp] treeChildCRUD 수정 메소드 추가

--- src/utils/tree/treeChildCRUD.ts
+++ src/utils/tree/treeChildCRUD.ts
@@ -1,9 +1,11 @@
 import { Tree } from '@/src/models/tree.model';
+import { pipe } from 'fp-ts/lib/function';
 import _ from 'lodash';
 import { getEmptyArrayIfNotArray } from '../common/arrayUtil';
 import { sortingTreeByTreeName } from './treeUtil';
 
 type CUDFromParentFn = (parentTree: Tree, childTree: Tree) => Tree;
+type RFromParentFn = (parentTree: Tree, childTreeId: number) => Tree | undefined;
 
 export const addChildToParent: CUDFromParentFn = (parentTree, childTree) => {
   const copyParentTree = _.cloneDeep(parentTree);
@@ -15,10 +17,13 @@ export const addChildToParent: CUDFromParentFn = (parentTree, childTree) => {
   return copyParentTree;
 }
 
-export const findChildFromParentById = (
-  parentTree: Readonly<Tree>,
-  childTreeId: number
-): Tree | undefined => _.find(parentTree.treeChildren, { 'treeId': childTreeId });
+export const findChildFromParentById: RFromParentFn = (parentTree, childTreeId) => _.find(parentTree.treeChildren, { 'treeId': childTreeId });
+
+export const replaceChildFromParent: CUDFromParentFn = (parentTree, childTree) => pipe(
+  parentTree,
+  removeChildFromParentCR(childTree),
+  addChildToParentCR(childTree),
+);
 
 export const removeChildFromParent: CUDFromParentFn = (parentTree, childTree) => {
   const copyParentTree = _.cloneDeep(parentTree);
