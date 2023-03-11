425d1a548c55fde37adc889b89488aa432b197d6
woogieReal <woogiereal@gmail.com>
2023-03-12 00:19:54 +0900

    [트리] syntax highlighting 목록 추가

--- src/utils/common/parserUtil.ts
+++ src/utils/common/parserUtil.ts
@@ -6,6 +6,8 @@ import remarkBreaks from 'remark-breaks'
 import rehypeRaw from 'rehype-raw'
 import remarkGfm from 'remark-gfm'
 import rehypePrism from 'rehype-prism'
+import 'prismjs/plugins/line-numbers/prism-line-numbers.css'
+import 'prismjs/plugins/autoloader/prism-autoloader.js'
 
 const parseMd = (contentMd: string | undefined) => {
   return unified()
@@ -14,9 +16,49 @@ const parseMd = (contentMd: string | undefined) => {
     .use(remarkGfm)
     .use(remarkRehype, {allowDangerousHtml: true})
     .use(rehypeRaw)
-    .use(rehypePrism)
+    .use(rehypePrism, {
+      plugins: ['autoloader'] // 작동 안함
+    })
     .use(rehypeStringify)
     .processSync(contentMd).value as string;
 }
 
 export default parseMd;
+
+import 'prismjs/components/prism-bash';
+import 'prismjs/components/prism-batch';
+import 'prismjs/components/prism-c';
+import 'prismjs/components/prism-css';
+import 'prismjs/components/prism-csv';
+import 'prismjs/components/prism-dart';
+import 'prismjs/components/prism-docker';
+import 'prismjs/components/prism-dot';
+import 'prismjs/components/prism-git';
+import 'prismjs/components/prism-go';
+import 'prismjs/components/prism-graphql';
+import 'prismjs/components/prism-groovy';
+import 'prismjs/components/prism-haskell';
+import 'prismjs/components/prism-hcl';
+import 'prismjs/components/prism-java';
+import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-json';
+import 'prismjs/components/prism-kotlin';
+import 'prismjs/components/prism-less';
+import 'prismjs/components/prism-log';
+import 'prismjs/components/prism-markdown';
+import 'prismjs/components/prism-markup';
+import 'prismjs/components/prism-mongodb';
+import 'prismjs/components/prism-pascal';
+import 'prismjs/components/prism-perl';
+import 'prismjs/components/prism-python';
+import 'prismjs/components/prism-ruby';
+import 'prismjs/components/prism-rust';
+import 'prismjs/components/prism-sas';
+import 'prismjs/components/prism-sass';
+import 'prismjs/components/prism-scala';
+import 'prismjs/components/prism-scss';
+import 'prismjs/components/prism-sql';
+import 'prismjs/components/prism-toml';
+import 'prismjs/components/prism-typescript';
+import 'prismjs/components/prism-typoscript';
+import 'prismjs/components/prism-yaml';
