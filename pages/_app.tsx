0d78483b6ad4ba7318bc3745890ee9d40820a7c6
woogieReal <woogiereal@gmail.com>
2023-03-11 22:23:09 +0900

    [트리] code highlighting theme 변경

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
-import 'prismjs/themes/prism-coy.css'
-import 'prismjs/plugins/line-numbers/prism-line-numbers.css'
+import 'prismjs/themes/prism-okaidia.css';
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
