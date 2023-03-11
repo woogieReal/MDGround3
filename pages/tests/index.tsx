ef0414fe1c6da3f716b8f1d45a4c66236ac983b1
woogieReal <woogiereal@gmail.com>
2023-03-11 15:42:08 +0900

    [공통] CustomSnackbar 컴포넌트 테스트

--- pages/tests/index.tsx
+++ pages/tests/index.tsx
@@ -1,34 +1,15 @@
-import React, { useState, useEffect } from "react";
+import { CustomSnackbar, showSnackbar } from '@/components/common/module/customSnackbar';
+import * as React from 'react';
 
-const Home = () => {
-  
-  const [value, setValue] = useState<string>('');
-  const [isAutoSaved, setIsAutoSaved] = useState<boolean>(false);
-
-  useEffect(() => {
-    let timeout: NodeJS.Timeout;
-
-    timeout = setTimeout(() => {
-      setIsAutoSaved(true);
-    }, 5000);
-    if (value !== '') {
-    }
-
-    return () => clearTimeout(timeout);
-  }, [value]);
+const Test = () => {
+  // showSnackbar('!!!!');
 
   return (
     <div>
-      <input
-        value={value}
-        onChange={(e) => {
-          setValue(e.target.value);
-          setIsAutoSaved(false);
-        }}
-      />
-      {isAutoSaved && <p>It's been 5 seconds since the last change!</p>}
+      <button onClick={() => showSnackbar('???')} >클릭해!</button>
+      <CustomSnackbar />
     </div>
-  );
+  ) 
 }
 
-export default Home;
+export default Test;
