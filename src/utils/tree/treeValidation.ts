121f61ddd56c03872c7996efa3443d81a6c3748d
woogieReal <woogiereal@gmail.com>
2023-07-10 20:54:13 +0900

    feat: 다건 처리를 위해 트리 validation 메소드 함수 수정

--- src/utils/tree/treeValidation.ts
+++ src/utils/tree/treeValidation.ts
@@ -1,34 +1,40 @@
 import { Tree, TreeType } from "@/src/models/tree.model";
 import { ValidationResponse } from "@/src/models/validation.model";
 import { validateExecutor } from "../common/commonValidation";
-
-const checkInvalidTreeId = (treeId: number) => treeId <= 0;
-const checkInvalidTreeType = (treeType: TreeType) => ![...Object.values(TreeType)].includes(treeType);
-const checkInvalidTreeName = (treeName: string) => !treeName;
-const checkInvalidTreeContent = (treeContent: any) => typeof treeContent === 'undefined' || treeContent === null;
+import _ from 'lodash';
 
 type ValidateSingleTreeFn = (tree: Tree) => ValidationResponse<Tree>;
+const checkInvalidSingleTreeId = (treeId: number) => treeId <= 0;
+const checkInvalidSingleTreeType = (treeType: TreeType) => ![...Object.values(TreeType)].includes(treeType);
+const checkInvalidSingleTreeName = (treeName: string) => !treeName;
+const checkInvalidSingleTreeContent = (treeContent: any) => typeof treeContent === 'undefined' || treeContent === null;
+
+type ValidateMultiTreeFn = (treeList: Tree[]) => ValidationResponse<Tree[]>;
+const checkInvalidMultiTreeId = (treeIdList: number[]) => treeIdList.reduce((res, treeId) => _.concat(res, checkInvalidSingleTreeId(treeId)), [] as boolean[]).includes(true);
+const checkInvalidMultiTreeType = (treeTypeList: TreeType[]) => treeTypeList.reduce((res, treeType) => _.concat(res, checkInvalidSingleTreeType(treeType)), [] as boolean[]).includes(true);
+const checkInvalidMultiTreeName = (treeNameList: string[]) => treeNameList.reduce((res, treeName) => _.concat(res, checkInvalidSingleTreeName(treeName)), [] as boolean[]).includes(true);
+const checkInvalidMultiTreeContent = (treeContentList: any[]) => treeContentList.reduce((res, treeContent) => _.concat(res, checkInvalidSingleTreeContent(treeContent)), [] as boolean[]).includes(true);
 
 export const validateCreateTree: ValidateSingleTreeFn = (tree) => {
   return validateExecutor(tree, ['treeName'], (processedData: Tree): Array<boolean> => {
-    return [checkInvalidTreeType(processedData.treeType), checkInvalidTreeName(processedData.treeName)];
+    return [checkInvalidSingleTreeType(processedData.treeType), checkInvalidSingleTreeName(processedData.treeName)];
   })
 }
 
 export const validateEditContentTree: ValidateSingleTreeFn = (tree) => {
   return validateExecutor(tree, [], (processedData: Tree): Array<boolean> => {
-    return [checkInvalidTreeId(processedData.treeId), checkInvalidTreeContent(processedData.treeContent)];
+    return [checkInvalidSingleTreeId(processedData.treeId), checkInvalidSingleTreeContent(processedData.treeContent)];
   })
 }
 
-export const validateDeleteTree: ValidateSingleTreeFn = (tree) => {
-  return validateExecutor(tree, [], (processedData: Tree): Array<boolean> => {
-    return [checkInvalidTreeId(processedData.treeId), checkInvalidTreeType(processedData.treeType)];
+export const validateDeleteTree: ValidateMultiTreeFn = (treeList) => {
+  return validateExecutor(treeList, [], (processedData: Tree[]): Array<boolean> => {
+    return [checkInvalidMultiTreeId(_.map(processedData, 'treeId')), checkInvalidMultiTreeType(_.map(processedData, 'treeType'))];
   })
 }
 
 export const validateRenameTree: ValidateSingleTreeFn = (tree) => {
   return validateExecutor(tree, ['treeName'], (processedData: Tree): Array<boolean> => {
-    return [checkInvalidTreeId(processedData.treeId), checkInvalidTreeName(processedData.treeName)];
+    return [checkInvalidSingleTreeId(processedData.treeId), checkInvalidSingleTreeName(processedData.treeName)];
   })
 };
